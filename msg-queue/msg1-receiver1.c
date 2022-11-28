911e1a745ac3f51429ed03c892d2c2cd7a45e7a1
dlgksmf6789 <44254721+dlgksmf6789@users.noreply.github.com>
2022-11-28 16:39:52 +0900

    Add files via upload

---
+++ msg-queue/msg1-receiver1.c
@@ -0,0 +1,36 @@
+/* msg1-receiver1.c */
+/* only get the msg having the type = 1 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define  BUFF_SIZE   1024
+
+typedef struct {
+    long  data_type;
+    int   data_num;
+    char  data_buff[BUFF_SIZE];
+} msg_t;
+
+int main(void) {
+    int msqid;
+    msg_t data;
+
+    if(-1 == (msqid = msgget((key_t)1234, IPC_CREAT | 0666))) {   
+        perror("msgget() error");
+        exit(1);
+    }   
+
+    while(1) {
+        // get the messages only with type = 1
+        if (-1 == msgrcv(msqid, &data, sizeof(msg_t) - sizeof(long), 1, 0)) {
+            perror( "msgrcv() error");
+            exit(1);
+        }   
+        printf("%d - %s\n", data.data_num, data.data_buff);
+    }
+    return(0);
+}
